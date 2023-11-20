cb2b1747f38f20bf67354d7a0a891498f80b507f
956784 <146649071+956784@users.noreply.github.com>
2023-11-20 21:21:08 +0300

    Add files via upload

---
+++ lab5.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+
+int plus = 0;
+int minus = 0;
+Console.WriteLine("введите размер массива");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[num];
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] < 0)
+    {
+        minus += 1;
+        break;
+    }
+    else
+    {
+        plus += 1;
+        break;
+    }
+}
+if (minus > plus)
+{
+    Console.WriteLine("minus");
+}
+else
+{
+    Console.WriteLine("plus");
+}
